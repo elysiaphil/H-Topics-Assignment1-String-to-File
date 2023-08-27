f9724da7142c55b28e2fce3fec8d8646086b775b
king-of-barry <blebari1@hwemail.com>
2023-08-27 16:34:01 -0700

    This Repository contains my String to File and File to String projects
    There are still some issues (it can't turn the full line into a string) but this is what I have so far. I had to use ORacle Java for assistance

--- Functions.java
+++ Functions.java
@@ -1,5 +1,74 @@
-public class Functions {
+
+//credit given to JavaOracle and W3 School
+//Thank you Daniel for the assistance
+import java.util.*;
+
+import java.io.*; //import needed for files
+
+public class Functions { // apologies for the bad class name
     public static void main(String[] args) {
+
+        Functions.stringToFile();
+        System.out.println(Functions.fileToString());
+
+    }
+
+    public static void stringToFile() {
+
+        try {
+
+            Scanner keyboard = new Scanner(System.in);
+
+            String fileName;
+            System.out.print("Enter a file name: ");
+            fileName = keyboard.next();
+
+            PrintWriter pw = new PrintWriter(fileName); // found out online from Java Oracle and Danny and I disc
+            // makes a file under fileName
+
+            String words;
+
+            System.out.print("What would you like to have printed in the file? ");
+            words = keyboard.next();
+
+            System.out.println("OK! ITS MADE!");
+            pw.print(words);
+
+            pw.close(); // releases the info
+
+        } catch (Exception e) {
+
+            System.out.println(e);
+
+        }
+
+    }
+
+    public static String fileToString() {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        try {
+            String name;
+            System.out.print("Enter the file name: ");
+            name = keyboard.next();
+
+            FileReader fileReader = new FileReader(name); // learnt from Oracle Java and Daniel
+            BufferedReader br = new BufferedReader(fileReader); // learnt from Oracle Java and Daniel
+
+            String totalOutput = "";
+
+            totalOutput += br.readLine(); // learnt from oracle java and W3
+
+            System.out.println(totalOutput);
+            return totalOutput;
+
+        } catch (Exception e) {
+
+            System.out.println(e);
+            System.out.println("You may have entered the file's name incorrectly!");
+            return "";
+        }
 
     }
 }
